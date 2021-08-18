7227475703d9086a41455b87a318bd40d7601fd6
jcolandr <jcolandr@gmail.com>
2021-08-17 23:42:41 -0400

    provider block

--- terraform-aws-eks/vpc.tf
+++ terraform-aws-eks/vpc.tf
@@ -1,5 +1,6 @@
-
-
+provider "aws" {
+  region = var.region
+}
 
 data "aws_availability_zones" "available" {}
 
@@ -16,7 +17,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "education-vpc"
+  name                 = "eks-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
